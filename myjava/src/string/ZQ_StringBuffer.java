ea9b6cb5ccd171f760a93d650794f8eb64ad86c1
zhangqi <18810606053@163.com>
2018-11-10 20:56:35 +0800

    myjava

--- myjava/src/string/ZQ_StringBuffer.java
+++ myjava/src/string/ZQ_StringBuffer.java
@@ -6,11 +6,13 @@ package string;
  * @CreateDate: 2018/8/19$ 16:15$
  * @Version: 1.0
  * 线程安全
+ 
  */
 
+ //基本不用
 public class ZQ_StringBuffer {
     public static void main(String[] args) {
         StringBuffer stringBuffer = new StringBuffer();
-
+        System.out.println(stringBuffer);
     }
 }
